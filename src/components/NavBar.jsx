b7acaf47b60030d95fe07603070b2cb871b3a0ab
BRB3D <samuraix221@hotmail.com>
2022-05-29 22:13:34 -0400

    navbar mobile view unfinished

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,24 @@
-import React from 'react'; 
+import React from 'react';
+import './NavBar.scss'
 
  export default function NavBar() {
 
 
   return(
-    <div>
-
+    <div className="navbar">
+        <img alt="UX-NO logo" className="logo-navbar" src="../UX-NO.png"/>
+        <a>
+          <i className="home huge icon"/>
+        </a>
+        <a>
+          <i className="info huge icon" />
+        </a>
+        <a>
+          <i className="mail huge icon" />
+        </a>
+        <a>
+          Donate
+        </a>
     </div>
   )
  }
