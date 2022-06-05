0721c624e356c36d7b6a87d28598b922d0f98c3d
BRB3D <samuraix221@hotmail.com>
2022-06-05 19:42:01 -0400

    navbar responsive with Tim

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,19 +6,10 @@ import './NavBar.scss'
 
   return(
     <div className="navbar">
-        <img alt="UX-NO logo" className="logo-navbar" src="../UX-NO.png"/>
-        <a>
-          <i className="home huge icon"/>
-        </a>
-        <a>
-          <i className="info huge icon" />
-        </a>
-        <a>
-          <i className="mail huge icon" />
-        </a>
-        <a>
-          Donate
-        </a>
+        <img alt="UX-NO logo" className="logo-navbar" src="../UX_NO.png"/>
+        <i class="fa-solid fa-house"></i>
+        <i class="fa-solid fa-circle-info"></i>
+        <i class="fa-solid fa-envelope"></i>
     </div>
   )
  }
